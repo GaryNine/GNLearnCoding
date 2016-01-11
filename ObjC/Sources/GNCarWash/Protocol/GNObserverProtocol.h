9a68a98cdabb94dbb97008f7f22f99dfe2058f1a
Igor Devyatko <garynine@gmail.com>
2016-01-11 16:20:04 +0200

    Rename GNObservable

---
+++ ObjC/Sources/GNCarWash/Protocol/GNObserverProtocol.h
@@ -0,0 +1,18 @@
+//
+//  GNObserverProtocol.h
+//  GNLearnCoding
+//
+//  Created by Igor Devyatko on 1/10/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GNObserverProtocol <NSObject>
+
+@optional
+- (void)employeeDidBecomeFree:(id)employee;
+- (void)employeeDidBecomeWork:(id)employee;
+- (void)employeeDidBecomeProcessing:(id)employee;
+
+@end
