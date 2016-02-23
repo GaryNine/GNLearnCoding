fe40598dfb4dffbe2cb8b1a9754e01a1f6f82078
Igor Devyatko <garynine@gmail.com>
2016-02-23 18:56:42 +0200

    Add UITableView extensions

---
+++ LearnUI/Source/Library/Categories/UITableView/UITableView+GNExtensions.h
@@ -0,0 +1,15 @@
+//
+//  UITableView+GNExtensions.h
+//  GNLearnUI
+//
+//  Created by Igor Devyatko on 2/21/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (GNExtensions)
+
+- (id)cellWithClass:(Class)cls;
+
+@end
