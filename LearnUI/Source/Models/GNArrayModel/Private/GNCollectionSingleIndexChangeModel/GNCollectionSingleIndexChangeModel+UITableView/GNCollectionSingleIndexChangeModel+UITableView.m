8f5d841f4c4fdc3ad0fb05f0dd40fdd25949896f
Igor Devyatko <garynine@gmail.com>
2016-03-09 19:19:51 +0200

    Add GNCollectionSingleIndexChangeModel+UITableView

---
+++ LearnUI/Source/Models/GNArrayModel/Private/GNCollectionSingleIndexChangeModel/GNCollectionSingleIndexChangeModel+UITableView/GNCollectionSingleIndexChangeModel+UITableView.m
@@ -0,0 +1,61 @@
+//
+//  GNCollectionSingleIndexChangeModel+UITableView.m
+//  GNLearnUI
+//
+//  Created by Igor Devyatko on 3/9/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import "UIKit/UIKit.h"
+
+#import "GNCollectionSingleIndexChangeModel+UITableView.h"
+
+#import "UITableView+GNExtensions.h"
+#import "NSIndexPath+GNExtensions.h"
+
+@implementation GNCollectionSingleIndexChangeModel (UITableView)
+
+@dynamic indexPath;
+
+- (NSIndexPath *)indexPath {
+    return [NSIndexPath indexPathForRow:self.index];
+}
+
+- (void)updateTableView:(UITableView *)tableView {
+    NSIndexPath *indexPath = self.indexPath;
+    NSArray *indexPaths = @[indexPath];
+    
+    [tableView updateTableViewWithBlock:^(UITableView *object) {
+        switch (self.changeType) {
+            case kGNChangeTypeObjectAdded:
+                [tableView insertRowsAtIndexPaths:indexPaths
+                                 withRowAnimation:UITableViewRowAnimationTop];
+                break;
+                
+            case kGNChangeTypeObjectRemoved:
+                [tableView deleteRowsAtIndexPaths:indexPaths
+                                 withRowAnimation:UITableViewRowAnimationFade];
+                break;
+                
+            case kGNChangeTypeObjectInserted:
+                [tableView insertRowsAtIndexPaths:indexPaths
+                                 withRowAnimation:UITableViewRowAnimationTop];
+                break;
+                
+            case kGNChangeTypeObjectReplaced:
+                [tableView reloadRowsAtIndexPaths:indexPaths
+                                 withRowAnimation:UITableViewRowAnimationTop];
+                
+            default:
+                break;
+        }
+    }];
+    
+    if (kGNChangeTypeObjectAdded == self.changeType) {
+        [tableView scrollToRowAtIndexPath:indexPath
+                         atScrollPosition:UITableViewScrollPositionBottom
+                                 animated:YES];
+    }
+}
+
+@end
