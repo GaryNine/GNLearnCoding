ddb6577a651e7ba2638d39827996899ace562bb6
Igor Devyatko <garynine@gmail.com>
2016-03-09 19:01:40 +0200

    Add NSIndexPath category

---
+++ LearnUI/Source/Library/Categories/NSIndexPath/NSIndexPath+GNExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSIndexPath+GNExtensions.h
+//  GNLearnUI
+//
+//  Created by Igor Devyatko on 3/9/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSIndexPath (GNExtensions)
+
++ (instancetype)indexPathForRow:(NSUInteger)rowIndex;
+
+@end
