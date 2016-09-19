b626474dc49fa33d2f021fe14dae823019717c8e
Igor Devyatko <garynine@gmail.com>
2016-09-19 18:59:39 +0300

    Add GNImageModelCache class

---
+++ LearnUI/Source/Library/Classes/GNImageModelCache/GNImageModelCache.h
@@ -0,0 +1,13 @@
+//
+//  GNImageModelCache.h
+//  GNLearnUI
+//
+//  Created by Igor Devyatko on 9/17/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import "GNObservableObject.h"
+
+@interface GNImageModelCache : NSObject
+
+@end
---
+++ LearnUI/Source/Library/Classes/GNImageModelCache/GNImageModelCache.m
@@ -0,0 +1,13 @@
+//
+//  GNImageModelCache.m
+//  GNLearnUI
+//
+//  Created by Igor Devyatko on 9/17/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import "GNImageModelCache.h"
+
+@implementation GNImageModelCache
+
+@end
