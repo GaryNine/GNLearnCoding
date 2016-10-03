a2492544ac245eb32e4c4d2c146e6da38a319b76
Igor Devyatko <garynine@gmail.com>
2016-10-03 18:56:52 +0300

    Add login and friends context

---
+++ LearnUI/Source/Library/Contexts/GNFacebookFriends/GNFacebookFriends.m
@@ -0,0 +1,13 @@
+//
+//  GNFacebookFriends.m
+//  GNLearnUI
+//
+//  Created by Igor Devyatko on 10/3/16.
+//  Copyright © 2016 IDAP College. All rights reserved.
+//
+
+#import "GNFacebookFriends.h"
+
+@implementation GNFacebookFriends
+
+@end
