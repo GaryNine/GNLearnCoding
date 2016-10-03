e71a9166024581fa2343a55e753ac29cdc10e00a
Igor Devyatko <garynine@gmail.com>
2016-10-03 18:55:07 +0300

    Add UINavigationController to GNAppDelegate

--- LearnUI/GNLearnUI/GNAppDelegate.m
+++ LearnUI/GNLearnUI/GNAppDelegate.m
@@ -8,7 +8,11 @@
 
 #import "GNAppDelegate.h"
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
 #import "UIWindow+GNExtensions.h"
+
+#import "GNLoginViewController.h"
 
 #import "GNSquareView.h"
 #import "GNSquareViewController.h"
@@ -31,16 +35,30 @@
     UIWindow *window = [UIWindow window];
     self.window = window;
 //    GNSquareViewController *controller = [GNSquareViewController new];
-    GNUsersViewController *controller = [GNUsersViewController new];
-    window.rootViewController = controller;
+//    GNUsersViewController *controller = [GNUsersViewController new];
+    GNLoginViewController *controller = [GNLoginViewController new];
     
-    GNUsers *users = [GNUsers new];
-    controller.users = users;
-    self.users = users;
+    UINavigationController *navigationController = [[UINavigationController alloc ] initWithRootViewController:controller];
+    window.rootViewController = navigationController;
+    
+//    GNUsers *users = [GNUsers new];
+//    controller.users = users;
+//    self.users = users;
     
     [window makeKeyAndVisible];
     
-    return YES;
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                    didFinishLaunchingWithOptions:launchOptions];;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -58,6 +76,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     GNPrintMethod;
+    
+    [FBSDKAppEvents activateApp];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
