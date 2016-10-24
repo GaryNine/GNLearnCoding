5a497d54d8b9464f7c47f216faeabde8885fe3d7
Igor Devyatko <garynine@gmail.com>
2016-10-24 15:25:24 +0300

    Add GNFriendDetailContext

--- LearnUI/Source/Library/Contexts/GNFriendDetailContext/GNFriendDetailContext.m
+++ LearnUI/Source/Library/Contexts/GNFriendDetailContext/GNFriendDetailContext.m
@@ -7,7 +7,43 @@
 //
 
 #import "GNFriendDetailContext.h"
+#import "GNUser.h"
+#import "GNConstants.h"
 
 @implementation GNFriendDetailContext
+
+#pragma mark -
+#pragma mark Public
+
+- (NSString *)graphPath {
+    GNUser *user = self.model;
+    return user.userID;
+}
+
+- (NSDictionary *)parameteres {
+    NSString *fields = [NSString stringWithFormat:@"%@,%@,%@,%@,%@,%@{%@}",
+                        kGNFirstNameKey,
+                        kGNLastNameKey,
+                        kGNGenderKey,
+                        kGNEmailKey,
+                        kGNBirthdayKey,
+                        kGNPictureKey,
+                        kGNUrlKey];
+    
+    return @{kGNFieldsKey : fields};
+}
+
+- (void)fillModelWithResult:(NSDictionary *)result {
+    GNUser *user = self.model;
+    
+    user.firstName = result[kGNFirstNameKey];
+    user.lastName = result[kGNLastNameKey];
+    user.gender = result[kGNGenderKey];
+    user.email = result[kGNEmailKey];
+    user.birthday = result[kGNBirthdayKey];
+    
+    NSString *urlString = result[kGNPictureKey][kGNDataKey][kGNUrlKey];
+    user.imageURL = [NSURL URLWithString:urlString];
+}
 
 @end
